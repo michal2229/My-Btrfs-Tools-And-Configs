9639f29fdaa58bb5d47cfa85f997be4b14a04130
Michal B <michal2229@gmail.com>
2017-08-24 21:45:01 +0200

    adding script to make batch of snapshots

---
+++ btrfs_make_snapshots.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+## Script to make batch of snapshots from given subvolumes.
+# It expects You to provide MNTDIR - a dir which contain mounted subvolumes,
+# whose names begin with '@' (i.e. @mountpoint), and SNAPDIR - a dir to put snapshots there.
+# Snapshots are made read-only.
+# Parameters of the script are the snapshot comment, for example:
+# >> btrfs_make_snapshots.sh This is a comment of a snapshots
+# Snapshots are placed in subfolders, which name has '_' instead of '@' (i.e. _mountpoint).
+# Snapshots are named as follows: name;date;comment, for example: @mountpoint;2017-05-04_12-34;This_is_comment
+
+
+MNTDIR="/btrfs-root/mountpoints"
+SNAPDIR="/btrfs-root/snapshots"
+
+DATE=$(date "+%y-%m-%d_%H-%M")
+COMMENT="$(echo $@ | sed 's/ /_/g')"
+
+for i in $(ls $MNTDIR); do
+    j=$(echo $i | sed "s/@/_/g")
+    snapdir1="$SNAPDIR/$j"
+    sudo mkdir $snapdir1 || true
+    sudo btrfs sub snap $MNTDIR/$i "$snapdir1/$i;$DATE;$COMMENT" -r
+done
